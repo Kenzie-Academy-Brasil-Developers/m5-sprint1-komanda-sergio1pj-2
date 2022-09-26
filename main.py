4ae0d62c0f0eb6ac39957df4961912437a6e96e8
Sergio Pereira <72481029+sergio1pj@users.noreply.github.com>
2022-09-26 10:06:24 -0300

    fix: alterando imports na main

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-from utils import json_handler
-from management import tab_handler
+from utils.json_handler import read_json, write_json
+from management.tab_handler import calculate_tab
 def main():
-    print(json_handler.write_json("menu.json", {"name": "CHURROS DO M5", "price": 5}))
-    print(json_handler.read_json("menu.json"))
+    print(write_json("menu.json", {"name": "CHURROS DO M5", "price": 5}))
+    print(read_json("menu.json"))
     table_1 = [{'id': 1, 'amount': 5}, {'id': 19, 'amount': 5}]
     table_2 = [
       {"id": 10, "amount": 3},
       {"id": 20, "amount": 2},
       {"id": 21, "amount": 5},
     ]
-    print(tab_handler.calculate_tab(table_1))
-    print(tab_handler.calculate_tab(table_2))
+    print(calculate_tab(table_1))
+    print(calculate_tab(table_2))
 if __name__ == "__main__":
     main()
     
