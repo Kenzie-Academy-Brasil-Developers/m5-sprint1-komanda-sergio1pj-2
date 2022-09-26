858269633bfd4d22196134ef1f2e74c8b696fb94
Sergio Pereira <72481029+sergio1pj@users.noreply.github.com>
2022-09-26 10:10:00 -0300

    fix: calculate_tab total

--- management/tab_handler.py
+++ management/tab_handler.py
@@ -5,5 +5,5 @@ def calculate_tab(data: list[dict]) -> dict:
     read = json_handler.read_json("menu.json")
     for p in data:
         list = [x for x in read if x["id"] == p["id"]]
-        total = list[0]["price"] * p["amount"] 
+        total += list[0]["price"] * p["amount"] 
     return dict(subtotal=total, created_at=datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
