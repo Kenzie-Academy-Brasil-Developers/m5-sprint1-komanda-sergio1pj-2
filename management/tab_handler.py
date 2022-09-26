fcdd9a17cffcd9e9962a729bd0598eac9c566197
Sergio Pereira <72481029+sergio1pj@users.noreply.github.com>
2022-09-26 10:13:03 -0300

    fix: inicialização total calculate_tab

--- management/tab_handler.py
+++ management/tab_handler.py
@@ -3,6 +3,7 @@ from datetime import datetime
 
 def calculate_tab(data: list[dict]) -> dict:
     read = json_handler.read_json("menu.json")
+    total = 0
     for p in data:
         list = [x for x in read if x["id"] == p["id"]]
         total += list[0]["price"] * p["amount"] 
